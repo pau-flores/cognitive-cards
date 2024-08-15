3ef554c4944e6831840278f1331e4bb99aebe544
Paulina Flores <paufr.2822@gmail.com>
2024-08-15 01:04:36 -0700

    Remove api version parameter to use latest

--- src/app/api/checkout_sessions.js
+++ src/app/api/checkout_sessions.js
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 // Initialize Stripe
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Format price for Stripe
 const formatAmountForStripe = (amount) => {
