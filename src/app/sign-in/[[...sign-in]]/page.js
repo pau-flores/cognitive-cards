a204eff79ceab5c49a5770ae8fc9d3e759d8f50c
Paulina Flores <paufr.2822@gmail.com>
2024-08-18 06:46:13 -0700

    Add global header component

---
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Container,
+  Box,
+  Typography,
+  AppBar,
+  Toolbar,
+  Button,
+} from "@mui/material";
+import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
+import Header from "@/app/components/Header";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          sx={{ textAlign: "center", my: 4 }}
+        >
+          <Typography variant="h4" component="h1" gutterBottom>
+            Sign In
+          </Typography>
+          <SignIn />
+        </Box>
+      </Container>
+    </>
+  );
+}
