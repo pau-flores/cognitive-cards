a900145b2c3d36bc199aa8b5d8676cd3e37c5910
Paulina Flores <paufr.2822@gmail.com>
2024-08-18 06:48:17 -0700

    Add links to navigation menu in header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { LuBrainCircuit } from "react-icons/lu";
 
 export default function Header() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: "#03045E" }}>
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
-          <Link href="/" style={{ textDecoration: "none", color: "white" }}>
-            Flashcard SaaS
+          <Link
+            href="/"
+            style={{
+              textDecoration: "none",
+              color: "white",
+            }}
+          >
+            <LuBrainCircuit /> CognitiveCards
           </Link>
         </Typography>
         <SignedOut>
@@ -21,11 +28,18 @@ export default function Header() {
         </SignedOut>
         <SignedIn>
           <Link
+            href="/generate"
+            passHref
+            style={{ textDecoration: "none", color: "white", margin: "0 30px" }}
+          >
+            Generate
+          </Link>
+          <Link
             href="/flashcards"
             passHref
-            style={{ textDecoration: "none", color: "white", margin: "0 20px" }}
+            style={{ textDecoration: "none", color: "white", margin: "0 30px" }}
           >
-            My Flashcards
+            My FLashcards
           </Link>
           <UserButton />
         </SignedIn>
